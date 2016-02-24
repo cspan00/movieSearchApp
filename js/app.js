ad114d3d007b5dec7b57699888c1c4f0b9158def
Rachel Parri <erachelparri@gmail.com>
2016-02-24 13:41:44 -0700

    movies showing in list

--- js/app.js
+++ js/app.js
@@ -13,12 +13,21 @@ app.config(function($routeProvider, $locationProvider){
 
 app.controller("ResultsController", function($scope, $location, $routeParams, $http){
   // $scope.output = $routeParams.title;
-
-
   var title = $routeParams.title
+  var movies = [];
+  $scope.movies = movies; 
+
   $http.get('http://www.omdbapi.com/?s='+title+'&y=&plot=short&r=json').then(function(data){
-    $scope.output = data;
-    console.log(data);
+    // data["data"]["Search"]["Title"]
+    data["data"]["Search"].forEach(function(movie, i){
+      var movie = {};
+      movie.title = data["data"]["Search"][i]["Title"]
+      movie.year= data["data"]["Search"][i]["Year"]
+      movie.imdbID = data["data"]["Search"][i]["imdbID"]
+      movie.poster = data["data"]["Search"][i]["Poster"]
+      movies.push(movie)
+    })
+    console.log(movies);
   });
 
 })
